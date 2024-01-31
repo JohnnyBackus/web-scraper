307b55e442f67893d27172538da273dd563c1988
JohnnyBackus <john.p.backus@gmail.com>
2024-01-30 18:01:18 -0800

    functions updated to take url as argument,readme updated

--- scrape_citations.py
+++ scrape_citations.py
@@ -1,35 +1,25 @@
 from bs4 import BeautifulSoup
+import requests
 
 
-
-
-def get_citations_needed(markup):
+def get_citations_needed(markup): #shared function to find "citations needed"
     soup = BeautifulSoup(markup,"html.parser")
-    citations_needed_soup = soup.find_all("sup", "noprint Inline-Template Template-Fact")
-    # print("citation_needed_soup: ", citations_needed_soup)
-    return citations_needed_soup
+    citations_needed_soup = soup.find_all("sup", "noprint Inline-Template Template-Fact") #finds all <sup> tags with the class "noprint Inline-Template Template-Fact"
+    return citations_needed_soup #returns a list
 
 
-def get_citations_needed_count(markup):
-    count = len(get_citations_needed(markup))
-    # print(f"Number of citations needed: {count}")
+def get_citations_needed_count(url):
+    response = requests.get(url)
+    count = len(get_citations_needed(response.text)) #returns the length of the list, which is the number of "citation needed" <sup> tags
     return count
 
 
-def get_citations_needed_report(markup):
-    citation_sups = get_citations_needed(markup)
-    # print("citation_sups: ", citation_sups)
-    report = "Citations needed for this article: \n\n"
-    for sup in citation_sups:
-        sup_parent = sup.parent
-        # print("sup_parent: ", sup_parent)
-        # print("sup_parent.text: ", sup_parent.text)
-        report += sup_parent.text + "\n"
-   
+def get_citations_needed_report(url):
+    response = requests.get(url) 
+    citation_sups = get_citations_needed(response.text)
+    report = "Citations needed for this article: \n" 
+    
+    for sup in citation_sups: #iterate through the list of <sup> tags
+        sup_parent = sup.parent #get the parent of the <sup> tag
+        report += "\n" + sup_parent.text #add the text of the parent to the report and insert an empty line for spacing
     return report    
-  
-#     report = 
-#     for i in report:
-#         print(i.text)
-#         print(i.parent.parent.text)
-#         print("------------------------------------------------------")
--- scraper.py
+++ scraper.py
@@ -1,17 +1,12 @@
-import requests
 from scrape_citations import get_citations_needed_count, get_citations_needed_report
 
-
 url = "https://en.wikipedia.org/wiki/Valentine%27s_Day"
-response = requests.get(url)
-# print("response:", response.text)
 
-citations_count_results = get_citations_needed_count(response.text)
+citations_count_results = get_citations_needed_count(url)
 print("citations_count_results: ", citations_count_results)
 
-citations_report_results = get_citations_needed_report(response.text)
+citations_report_results = get_citations_needed_report(url)
 print("citations_report_results: ", citations_report_results)
 
-# with open("courses.txt","w") as f:
-#     f.write(citations_count_results)
-    # f.close()
+# with open("citations.txt","w") as f:
+#     f.write("The wikipedia article at " + url + " needs " + str(citations_count_results) + " citations. The paragraphs in need of citations are as follows: \n\n" + citations_report_results)
